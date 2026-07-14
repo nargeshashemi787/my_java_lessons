73a9fd8cab3771d1f2c89e4c5d1aa86761b61346
nargeshashemi787 <nargeshashemi787@gmail.com>
2026-07-14 15:47:52 +0330

    Update EndOperatorDemo code

---
+++ src/Session3/EndOperatorDemo.java
@@ -0,0 +1,16 @@
+package Session3;
+
+public class EndOperatorDemo {
+
+    static void main(String[] args) {
+        int i=10;
+       // i=i+10;
+      // i+=10;//خط 8و7 کاملا شبیه هم هستن
+
+        int j=i++; //عدد10 میشه
+        //int j=++i;//یه دونه اضافه میکنه
+
+        System.out.println(j);
+
+    }
+}
